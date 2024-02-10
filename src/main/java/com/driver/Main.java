e782b0b89a463db42e920a7542469b152f53d629
Henil61298 <henilshah9946@gmail.com>
2024-02-10 12:18:27 +0530

    initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+//        System.out.println(r.name); Error: java: name has private access in com.driver.RWOnly
+
+        r.setName("Henil");
+        System.out.println(r.getName());
+    }
 }
